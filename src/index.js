e5cc8ebd20fd0107b504f31ad6738258c5b3a44e
HarunobuTsutsumi <h.tsutsumi1203@gmail.com>
2020-05-09 23:39:51 +0900

    Board でマス目を並べる部分を、ハードコーディングではなく 2 つのループを使用するように書き換える。

--- src/index.js
+++ src/index.js
@@ -21,25 +21,15 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    const board = [];
+    for (let row = 0; row < 3; row ++) {
+      const boardRow = [];
+      for (let col = 0; col < 3; col ++) {
+        boardRow.push(this.renderSquare(row * 3 + col));
+      }
+      board.push(<div className="board-row">{boardRow}</div>);
+    }
+    return board;
   }
 }
 
